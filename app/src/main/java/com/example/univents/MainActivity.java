ed73c1c326042535a3410a46acf2eec7bccf8f98
sjai0024 <sjai0024@student.monash.edu>
2022-05-05 02:39:46 +1000

    Commit into new Project

---
+++ app/src/main/java/com/example/univents/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.univents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.univents.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        binding.culturalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EventScreen.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
