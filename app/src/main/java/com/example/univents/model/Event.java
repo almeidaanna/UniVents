b0a493c9c21f9bdd82c6cf872501317b29a0af23
Anna Almeida <aalm0032@student.monash.edu>
2022-05-07 16:52:18 +1000

    recycler view

---
+++ app/src/main/java/com/example/univents/model/Event.java
@@ -0,0 +1,70 @@
+package com.example.univents.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Event {
+    private String eventName;
+    private String eventDate;
+    private String eventDay;
+    private String eventDetail;
+
+    public Event(String name, String date, String day, String detail) {
+        eventName = name;
+        eventDate = date;
+        eventDay = day;
+        eventDetail = detail;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String name) {
+        eventName = name;
+    }
+
+    public String getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(String date) {
+        eventDate = date;
+    }
+
+    public String getEventDay() {
+        return eventDay;
+    }
+
+    public void setEventDay(String day) {
+        eventDay = day;
+    }
+
+    public String getEventDetail() {
+        return eventDetail;
+    }
+
+    public void setEventDetail(String detail) {
+        eventDetail = detail;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "eventName='" + eventName + '\'' +
+                ", eventDate='" + eventDate + '\'' +
+                ", eventDay='" + eventDay + '\'' +
+                ", eventDetail='" + eventDetail + '\'' +
+                '}';
+    }
+
+    //remove this before actual deployment
+    public static ArrayList<Event> createEventList()
+    {
+        ArrayList<Event> eventList = new ArrayList<Event>();
+        eventList.add(new Event("event1","12/2/22", "Monday", "This is a dance event and starts at 4:00 pm be there"));
+        eventList.add(new Event("event2","13/2/22", "Tuesday", "This is a music event and starts at 4:00 pm be there"));
+        eventList.add(new Event("event3","14/2/22", "Wednesday", "This is a fashion event and starts at 4:00 pm be there"));
+        return eventList;
+    }
+}
