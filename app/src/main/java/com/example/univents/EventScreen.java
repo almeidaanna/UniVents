30bf255d2f9daaba737e0ea9eb2c236af380e1b6
Anna Almeida <aalm0032@student.monash.edu>
2022-05-07 20:33:07 +1000

    Navigation drawer changes

--- app/src/main/java/com/example/univents/EventScreen.java
+++ app/src/main/java/com/example/univents/EventScreen.java
@@ -31,13 +31,13 @@ public class EventScreen extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.appBarEventScreen.toolbar);
-        binding.appBarEventScreen.fab.setOnClickListener(new View.OnClickListener() {
+    /*    binding.appBarEventScreen.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        }); */
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
