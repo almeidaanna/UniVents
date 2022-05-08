3fbb7a715babea2a1554575d76e224721171885a
sjai0024 <sjai0024@student.monash.edu>
2022-05-09 03:08:05 +1000

    RecyclerView

--- app/src/main/java/com/example/univents/fragment/HomeFragment.java
+++ app/src/main/java/com/example/univents/fragment/HomeFragment.java
@@ -4,21 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+
+import com.example.univents.adapter.RecyclerViewAdapter;
 import com.example.univents.databinding.FragmentHomeBinding;
-import com.example.univents.ui.home.HomeViewModel;
+import com.example.univents.model.Event;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private RecyclerViewAdapter adapter;
+    private ArrayList<Event> events;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+
         return root;
     }
 
