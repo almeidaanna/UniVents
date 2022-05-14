7ca92a4e0204725f2ea35ea0f59f7809a8e26cad
sjai0024 <sjai0024@student.monash.edu>
2022-05-14 13:01:53 +1000

    added gradle dependencies for mapbox

--- app/src/main/java/com/example/univents/EventConfirmActivity.java
+++ app/src/main/java/com/example/univents/EventConfirmActivity.java
@@ -7,9 +7,14 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.univents.databinding.ActivityEventConfirmBinding;
+import com.mapbox.geojson.Point;
+import com.mapbox.maps.CameraOptions;
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
 
 public class EventConfirmActivity extends AppCompatActivity {
     private ActivityEventConfirmBinding binding;
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,14 @@ public class EventConfirmActivity extends AppCompatActivity {
         binding = ActivityEventConfirmBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+        final Point point = Point.fromLngLat(145.045837, -37.876823 );
+        mapView = findViewById(R.id.mapView);
+        CameraOptions cameraPosition = new CameraOptions.Builder()
+                .zoom(13.0)
+                .center(point)
+                .build();
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
+        mapView.getMapboxMap().setCamera(cameraPosition);
         binding.calendarBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
